dba5417e28825b36f0abbc0de8a6cb8b14bf820d
Becky <becsegal@gmail.com>
2016-12-06 18:40:06 +0000

    demos for lecture

---
+++ js/demo3.js
@@ -0,0 +1,10 @@
+// print the result of dividing the given parameters
+// does it look like the programmer understands division?
+function divide(numerator, denominator) {
+    console.log("numerator = " + numerator);
+    console.log("denominator = " + denominator);
+    return denominator/numerator;
+}
+var answer = divide(10, 2);
+console.log("answer = " + answer);
+console.log("Is that answer correct?");
