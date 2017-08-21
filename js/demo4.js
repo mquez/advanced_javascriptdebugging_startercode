5a275d28f5f0e9e85d64b2f5a26019cc8ae437f4
Peter Jablonski <pwjablonski@gmail.com>
2017-08-21 10:39:39 -0400

    Create demo4.js

---
+++ js/demo4.js
@@ -0,0 +1,12 @@
+
+function magicianMagic() {
+    return Math.floor((Math.random() * 100) + 1);
+}
+var secretNumber = magicianMagic();
+var input = prompt("Im thinking of a number between 1 - 100: ");
+
+if(secretNumber == input) {
+    alert("Impressive!");
+} else {
+	alert("Wrong!");
+}
